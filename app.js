a8e9fff24748d28731f26117bf392b8f604b6266
Nasir Ansari <115993017+Nasir-Technical@users.noreply.github.com>
2022-11-10 15:51:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+var fruit1 = "mango"
+var fruit2 = "Apple"
+var fruit3 = "pineapple"
+var fruit4 = "Banana"
+var fruit5 = "Graps"
+
+var fruits = ["mango","Apple","pineapple","Banana","Graps"];
+console.log(fruits[0])
+
+
+var a = ["a","b","c","d","e"]
+// a.push("s")
+// a.pop("s")
+// a.shift("s")
+// a.unshift("s")
+// var b = a.slice(1,2)
+// console.log(b)
+// a.splice(2,0,"sheriyar","Affan","Nasir","Shahzaib")
+// console.log(a)
+
+// var i = a.indexOf("d")
+// console.log(a[i])
+
+// var b = a.length;
+// console.log(b)
+
+for (var i = 0; i <10; i++){
+    console.log("2 x "+ (i+1) +"="+2*(i+1))
+}
